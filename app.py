dc8a9a61d20cb1d57149d316d2a514531e713bd8
Colin Buchheit <130728626+ColinBuchheit@users.noreply.github.com>
2024-04-10 12:53:20 -0500

    additional updates

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request, flash, send_from_directory, redirect, url_for
+from flask import Flask, render_template, request, flash, url_for
 import csv
-import os
 from datetime import datetime
 from dataFetcher import getStockData
 from graphGenerator import generate_graph
 from timeSeriesFunctions import getTimeSeriesFunction
-from main import preprocess_data  
+from main import preprocess_data
+import os
+from time import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
@@ -21,6 +22,8 @@ def get_stock_symbols():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     stock_symbols = get_stock_symbols()
+    graph_url = None  # Initialize graph_url to None
+
     if request.method == 'POST':
         symbol = request.form.get('symbol')
         chart_type = request.form.get('chart_type')
@@ -33,11 +36,9 @@ def index():
             "daily": "TIME_SERIES_DAILY",
             "weekly": "TIME_SERIES_WEEKLY",
             "monthly": "TIME_SERIES_MONTHLY"
-        }.get(time_series, "TIME_SERIES_DAILY")  # Adjust the variable name if necessary
+        }.get(time_series, "TIME_SERIES_DAILY")
 
-        # Consider including interval handling here for intraday data
         api_key = "V6BVQP0SPVJAVA6X"
-        # Adjust to use api_function mapped from the form input
         raw_data = getStockData(symbol, api_function, api_key)
 
         if not raw_data:
@@ -48,14 +49,12 @@ def index():
                 flash("No data available for the selected date range.")
             else:
                 graph_filename = generate_graph(filtered_data, chart_type, start_date, end_date, symbol)
-                # Ensure the generate_graph function saves the graph in the 'static/graphs' directory
-                graph_url = url_for('static', filename=f'graphs/{graph_filename}')
+            if graph_filename:  # Check if a filename was returned
+                graph_url = url_for('static', filename=graph_filename) + f"?{time()}"
 
-    # Pass graph_url to the template. If a graph was generated, it'll be displayed.
+
     return render_template('index.html', stock_symbols=stock_symbols, graph_url=graph_url)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-    
